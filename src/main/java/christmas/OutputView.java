3dcf2e4c337b93f9ef765230996ef8e7a5d7e415
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-14 14:30:03 +0900

    Feat(outputView) : `할인 전 총주문 금액`을 출력한다
    - order 클래스에서 `합산한 총액`을 인자로 받아 출력합니다

--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -14,4 +14,16 @@ public class OutputView {
     private static void printOrderHeader() {
         System.out.println("<주문 메뉴>");
     }
+
+    public static void printTotalPrice(Integer price) {
+        printWhiteSpace();
+        printTotalPriceHeader();
+        System.out.println(
+                String.format("%,d원", price)
+        );
+    }
+
+    private static void printTotalPriceHeader() {
+        System.out.println("<할인 전 총주문 금액>");
+    }
 }
