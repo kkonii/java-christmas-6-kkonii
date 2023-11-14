994a8d356c36c095e7a89620c2e09a6589453078
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-15 01:04:12 +0900

    Refactor : 이벤트가 적용되는 `총주문금액`의 조건을 수정

--- src/main/java/christmas/EventCondition.java
+++ src/main/java/christmas/EventCondition.java
@@ -3,12 +3,20 @@ package christmas;
 import java.util.function.BiFunction;
 
 public enum EventCondition {
-    D_DAY((date, totalPrice) -> date >= 1 && date <= 25),
-    WEEKDAY((date, totalPrice) -> (date % 7) >= 2 && (date % 7) <= 6),
-    WEEKEND((date, totalPrice) -> (date % 7) >= 0 && (date % 7) <= 1),
-    SPECIAL_DAY((date, totalPrice) -> (date % 7) == 3 || date == 25),
+    D_DAY((date, totalPrice) ->
+            date >= 1 && date <= 25
+                    && totalPrice >= 10_000),
+    WEEKDAY((date, totalPrice) ->
+            (date % 7) >= 2 && (date % 7) <= 6
+                    && totalPrice >= 10_000),
+    WEEKEND((date, totalPrice) ->
+            (date % 7) >= 0 && (date % 7) <= 1
+                    && totalPrice >= 10_000),
+    SPECIAL_DAY((date, totalPrice) ->
+            ((date % 7) == 3 || date == 25)
+                    && totalPrice >= 10_000),
     GIFT_DAY((date, totalPrice) -> totalPrice >= 120_000),
-    NONE((date, totalPrice) -> false);
+    NONE((date, totalPrice) -> totalPrice < 10_000);
 
     private final BiFunction<Integer, Integer, Boolean> condition;
 
