beb1ad021e6b1a16e561d8c3252075628fada106
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-15 04:39:10 +0900

    Feat(order) : 주문 수량이 `최대 수량`을 넘어가면 예외를 발생시킨다

--- src/main/java/christmas/Order.java
+++ src/main/java/christmas/Order.java
@@ -8,6 +8,7 @@ public class Order {
     private Map<Menu, Integer> order;
 
     private Order(Date date, Map<Menu, Integer> order) {
+        validateQuantities(order);
         this.date = date;
         this.order = order;
     }
@@ -20,6 +21,22 @@ public class Order {
                 ));
 
         return new Order(date, order);
+    }
+
+    private void validateQuantities(Map<Menu, Integer> order) {
+        int totalQuantities = calculateTotalQuantity(order);
+
+        if (isOverLimit(totalQuantities)) {
+            throw new IllegalArgumentException("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.");
+        }
+    }
+
+    private int calculateTotalQuantity(Map<Menu, Integer> order) {
+        return order.values().stream().mapToInt(Integer::intValue).sum();
+    }
+
+    private boolean isOverLimit(int quantities) {
+        return quantities > 20;
     }
 
     public Integer calculateTotalPrice() {
--- src/test/java/christmas/OrderTest.java
+++ src/test/java/christmas/OrderTest.java
@@ -4,10 +4,26 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class OrderTest {
+    private Map<String, Integer> menu;
+
+    @BeforeEach
+    public void setUp() {
+        menu = Map.of("티본스테이크", 24);
+    }
+
+    @Test
+    @DisplayName("::주문 수량이 최대 수량을 넘어갈 경우 예외가 발생합니다")
+    public void validateQuantities_ShouldThrowException_WhenOverLimit() {
+        assertThatThrownBy(() -> Order.of(Date.from(2), menu))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.");
+    }
+    
     @Test
     @DisplayName("::존재하지 않는 메뉴가 포함되어 있을 경우 예외가 발생합니다")
     public void Should_ThrowException_When_MenuDoesNotExist() {
