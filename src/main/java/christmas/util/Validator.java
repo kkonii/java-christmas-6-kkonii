b99d65e5f5c4fe57976904a3450531232cb18dec
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-28 12:37:22 +0900

    Remove(validator) : 사용되지 않는 클래스 제거

--- src/main/java/christmas/util/Validator.java
+++
@@ -1,9 +0,0 @@
-package christmas.util;
-
-import christmas.exception.InvalidOrderException;
-
-public class Validator {
-    public static <T> T validateDuplication() {
-        throw InvalidOrderException.of();
-    }
-}
