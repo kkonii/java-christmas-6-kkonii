62b6ac3991355d2d1ea567718b00546d1510eee1
kkonii <119216472+kkonii@users.noreply.github.com>
2023-12-09 17:27:12 +0900

    Feat : 총 주문 수량 검증 기능

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,15 +1,29 @@
 package christmas.domain;
 
+import christmas.exception.InvalidOrderException;
 import java.util.List;
 
 public class Order {
     private final List<OrderItem> orderItems;
 
     private Order(List<OrderItem> orderItems) {
+        validateQuantities(orderItems);
         this.orderItems = orderItems;
     }
 
     public static Order from(List<OrderItem> orderItems) {
         return new Order(orderItems);
+    }
+
+    private void validateQuantities(List<OrderItem> orderItems) {
+        if (calculateQuantities(orderItems) > 20) {
+            throw InvalidOrderException.of();
+        }
+    }
+
+    private int calculateQuantities(List<OrderItem> orderItems) {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getEachQuantity)
+                .sum();
     }
 }
--- src/main/java/christmas/domain/OrderItem.java
+++ src/main/java/christmas/domain/OrderItem.java
@@ -1,17 +1,29 @@
 package christmas.domain;
 
 import christmas.domain.menu.Menu;
+import christmas.exception.InvalidOrderException;
 
 public class OrderItem {
     private final Menu menu;
     private final int quantity;
 
     private OrderItem(Menu menu, int quantity) {
+        validateQuantity(quantity);
         this.menu = menu;
         this.quantity = quantity;
     }
 
     public static OrderItem of(String name, int quantity) {
         return new OrderItem(Menu.findMatchMenu(name), quantity);
+    }
+
+    private void validateQuantity(int quantity) {
+        if (quantity > 20) {
+            throw InvalidOrderException.of();
+        }
+    }
+
+    public int getEachQuantity() {
+        return quantity;
     }
 }
